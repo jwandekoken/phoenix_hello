2a09331cecc64a8ff5564a9a73b53d0d4eb1f35e
jwandekoken <julio.wandekoken@paketa.com.br>
2021-09-11 19:00:36 -0300

    experimenting with contexts

--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -18,10 +18,18 @@ defmodule HelloWeb.Router do
 
     get "/", PageController, :index
     resources "/users", UserController
+    resources "/sessions", SessionController, only: [:new, :create, :delete], singleton: true
+    resources "/pages", PageController
     # get "/show", PageController, :show
     # get "/hello", HelloController, :index
     # get "/hello/:messenger", HelloController, :show
   end
+
+  scope "/cms", HelloWeb.CMS, as: :cms do
+    pipe_through [:browser, :authenticate_user]
+    resources "/pages", PageController
+  end
+
 
   # Other scopes may use custom stacks.
   # scope "/api", HelloWeb do
@@ -43,6 +51,18 @@ defmodule HelloWeb.Router do
     scope "/" do
       pipe_through :browser
       live_dashboard "/dashboard", metrics: HelloWeb.Telemetry
+    end
+  end
+
+  defp authenticate_user(conn, _) do
+    case get_session(conn, :user_id) do
+      nil ->
+        conn
+        |> Phoenix.Controller.put_flash(:error, "Login required")
+        |> Phoenix.Controller.redirect(to: "/")
+        |> halt()
+      user_id ->
+        assign(conn, :current_user, Hello.Accounts.get_user!(user_id))
     end
   end
 end
